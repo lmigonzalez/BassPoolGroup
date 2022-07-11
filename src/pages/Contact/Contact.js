6d0daae5975eb61c575ac4807b0b6145366b9671
lmigonzalez <lmigonzalezs@gmail.com>
2022-07-11 19:50:03 -0400

    contact error and sent message handler

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -3,6 +3,8 @@ import React, {useState} from "react";
 import axios from "axios";
 
 import { Form, Button } from "react-bootstrap";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+
 
 import "./Contact.css";
 
@@ -18,11 +20,103 @@ function Contact() {
   }
   const [userData, setUserData] = useState(initialData)
 
+  const [lastNameDisabled, setLastNameDisabled] = useState(true)
+  const [emailDisabled, setEmailDisabled] = useState(true)
+  const [zipCodeDisabled, setZipCodeDisabled] = useState(true)
+  const [phoneDisabled, setPhoneDisabled] = useState(true)
+  const [messageDisabled, setMessageDisabled] = useState(true)
+  const [submitDisabled, setSubmitDisabled] = useState(true)
+
+  const [sentSuccess, setSentSuccess] = useState(false)
+  const [sentError, setSentError] = useState(false)
+
+  const handleInputError = (e) =>{
+      if(e.target.name === 'first_name'){
+        if(e.target.value.trim().length > 2){
+          setLastNameDisabled(false)
+        }else{
+          setLastNameDisabled(true)
+          setEmailDisabled(true)
+          setZipCodeDisabled(true)
+          setPhoneDisabled(true)
+          setMessageDisabled(true)
+          setSubmitDisabled(true)
+        }
+      }
+
+      if(e.target.name === 'last_name'){
+        if(e.target.value.trim().length > 2){
+          setEmailDisabled(false)
+        }else{
+          setEmailDisabled(true)
+          setZipCodeDisabled(true)
+          setPhoneDisabled(true)
+          setMessageDisabled(true)
+          setSubmitDisabled(true)
+        }
+      }
+
+      if(e.target.name === 'email'){
+        if(e.target.value.trim().length > 4 && e.target.value.includes('@') && e.target.value.includes('.')){
+          setZipCodeDisabled(false)
+          
+        }else{
+          setZipCodeDisabled(true)
+          setPhoneDisabled(true)
+          setMessageDisabled(true)
+          setSubmitDisabled(true)
+        }
+  
+      }
+      if(e.target.name === 'zip_code'){
+        if(e.target.value.trim().length > 4){
+          setPhoneDisabled(false)
+        }else{
+          setPhoneDisabled(true)
+          setMessageDisabled(true)
+          setSubmitDisabled(true)
+        }
+      }
+
+      if(e.target.name === 'phone_number'){
+        if(e.target.value.trim().length > 9){
+          setMessageDisabled(false)
+        }else{
+          setMessageDisabled(true)
+          setSubmitDisabled(true)
+        }
+      }
+
+      if(e.target.name === 'phone_number'){
+        if(e.target.value.trim().length > 9){
+          setSubmitDisabled(false)
+        }else{
+          setSubmitDisabled(true)
+        }
+      }
+
+      
+  }
+
+  const closeSuccessMessage = () =>{
+    setTimeout(() => {
+      setSentSuccess(false)
+    }, 5000)
+  }
+
+  const closeErrorMessage = () =>{
+    setTimeout(() => {
+      setSentError(false)
+    }, 5000)
+  }
+
   const handleChange = (e) =>{
     setUserData({
       ...userData, [e.target.name]: e.target.value
     })
-
+    handleInputError(e)
+    console.log(e.target.name)
+  
   }
 
   const handleRadioTrue = () =>{
@@ -43,14 +137,18 @@ function Contact() {
     
 
 
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/createmessage/`, userData)
+    axios.post(`http://localhost:3001/api/createmessage`, userData)
     .then(res=>{
-      console.log(res)
+      setSentSuccess(true)
+      closeSuccessMessage()
     }).catch(err=>{
-      console.log(err)
+      setSentError(true)
+      closeErrorMessage()
     })
 
     setUserData(initialData)
+    
+    
   
   }
 
@@ -62,30 +160,30 @@ function Contact() {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="required">First Name</Form.Label>
-          <Form.Control type="text" name='first_name' value={userData.first_name} onChange={handleChange}/>
+          <Form.Control type="text" name='first_name' value={userData.first_name} onChange={handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label className="required">Last Name</Form.Label>
-          <Form.Control type="text" name='last_name' value={userData.last_name} onChange={handleChange}/>
+          <Form.Control type="text" name='last_name' value={userData.last_name} onChange={handleChange} disabled={lastNameDisabled}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className="required">Email</Form.Label>
-          <Form.Control type="email" name='email' value={userData.email} onChange={handleChange}/>
+          <Form.Control type="email" name='email' value={userData.email} onChange={handleChange} disabled={emailDisabled}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className="required">Zip Code</Form.Label>
-          <Form.Control type="number" name='zip_code' value={userData.zip_code} onChange={handleChange}/>
+          <Form.Control type="number" name='zip_code' value={userData.zip_code} onChange={handleChange} disabled={zipCodeDisabled}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className="required">Phone</Form.Label>
-          <Form.Control type="number" name='phone_number' value={userData.phone_number} onChange={handleChange}/>
+          <Form.Control type="number" name='phone_number' value={userData.phone_number} onChange={handleChange} disabled={phoneDisabled}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Do you have any questions you want to ask?</Form.Label>
-          <Form.Control as="textarea" name='message' value={userData.message} onChange={handleChange}/>
+          <Form.Control as="textarea" name='message' value={userData.message} onChange={handleChange} disabled={messageDisabled}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -94,7 +192,13 @@ function Contact() {
             onClick={handleRadioTrue}  />
             <Form.Check reverse label="No" name="group1" type="radio" onClick={handleRadioFalse}/>
             </Form.Group>
-          <Button type="submit">Submit</Button>
+        
+            <div className="btn-container">
+              <p className={sentSuccess? 'success-message' : 'success-message inactive'}><AiOutlineCheck className="check-icon"/> Message sent successfully</p>
+
+              <p className={sentError? 'error-message' : 'error-message inactive'}><AiOutlineClose className="check-icon"/> Your message was not sent. Please try again</p>
+              <Button type="submit" disabled={submitDisabled}>Submit</Button>
+            </div>
         </Form>
     </section>
   );
